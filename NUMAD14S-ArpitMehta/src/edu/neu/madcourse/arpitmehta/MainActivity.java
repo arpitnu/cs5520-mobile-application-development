26d01cef35ff1fa8f5f5218d180779ea4eb6289a
arpitm <arpit231087@gmail.com>
2014-01-14 22:04:39 -0500

    Changes:   1) manifest - changed to handle the shortcut creation when the app is installed.   2) Sudoku.java - tried to handle the dialog box state. still not working properly when the user clicks outside the dialog box and rotates the phone.

--- NUMAD14S-ArpitMehta/src/edu/neu/madcourse/arpitmehta/MainActivity.java
+++ NUMAD14S-ArpitMehta/src/edu/neu/madcourse/arpitmehta/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		setTitle(R.string.hello_mad_name);
 	}
 
 	@Override
--- NUMAD14S-ArpitMehta/src/edu/neu/madcourse/arpitmehta/sudoku/Sudoku.java
+++ NUMAD14S-ArpitMehta/src/edu/neu/madcourse/arpitmehta/sudoku/Sudoku.java
@@ -5,7 +5,7 @@
  * courses, books, articles, and the like. Contact us if you are in doubt.
  * We make no guarantees that this code is fit for any purpose. 
  * Visit http://www.pragmaticprogrammer.com/titles/eband3 for more book information.
-***/
+ ***/
 package edu.neu.madcourse.arpitmehta.sudoku;
 
 import edu.neu.madcourse.arpitmehta.R;
@@ -14,6 +14,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -22,95 +23,121 @@ import android.view.View;
 import android.view.View.OnClickListener;
 
 public class Sudoku extends Activity implements OnClickListener {
-   private static final String TAG = "Sudoku";
-   
-   /** Called when the activity is first created. */
-   @Override
-   public void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.main);
+	private static final String TAG = "Sudoku";
+//	private static boolean newGameClick = false;
+	private static boolean newGameDialogOpen = false;
 
-      // Set up click listeners for all the buttons
-      View continueButton = findViewById(R.id.continue_button);
-      continueButton.setOnClickListener(this);
-      View newButton = findViewById(R.id.new_button);
-      newButton.setOnClickListener(this);
-      View aboutButton = findViewById(R.id.about_button);
-      aboutButton.setOnClickListener(this);
-      View exitButton = findViewById(R.id.exit_button);
-      exitButton.setOnClickListener(this);
-   }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
-   @Override
-   protected void onResume() {
-      super.onResume();
-      Music.play(this, R.raw.main);
-   }
+		// Set up click listeners for all the buttons
+		View continueButton = findViewById(R.id.continue_button);
+		continueButton.setOnClickListener(this);
+		View newButton = findViewById(R.id.new_button);
+		newButton.setOnClickListener(this);
+		View aboutButton = findViewById(R.id.about_button);
+		aboutButton.setOnClickListener(this);
+		View exitButton = findViewById(R.id.exit_button);
+		exitButton.setOnClickListener(this);
+	}
 
-   @Override
-   protected void onPause() {
-      super.onPause();
-      Music.stop(this);
-   }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Music.play(this, R.raw.main);
 
-   public void onClick(View v) {
-      switch (v.getId()) {
-      case R.id.continue_button:
-         startGame(Game.DIFFICULTY_CONTINUE);
-         break;
-         // ...
-      case R.id.about_button:
-         Intent i = new Intent(this, About.class);
-         startActivity(i);
-         break;
-      // More buttons go here (if any) ...
-      case R.id.new_button:
-         openNewGameDialog();
-         break;
-      case R.id.exit_button:
-         finish();
-         break;
-      }
-   }
-   
-   @Override
-   public boolean onCreateOptionsMenu(Menu menu) {
-      super.onCreateOptionsMenu(menu);
-      MenuInflater inflater = getMenuInflater();
-      inflater.inflate(R.menu.menu, menu);
-      return true;
-   }
+		// Check the sate of new game dialog. If it is true then call
+		// openNewGameDialog function
+		if (false != newGameDialogOpen) {
+			// open a new game dialog
+			openNewGameDialog();
 
-   @Override
-   public boolean onOptionsItemSelected(MenuItem item) {
-      switch (item.getItemId()) {
-      case R.id.settings:
-         startActivity(new Intent(this, Prefs.class));
-         return true;
-      // More items go here (if any) ...
-      }
-      return false;
-   }
+//			// Reset flag
+//			newGameDialogOpen = false;
+		}
+	}
 
-   /** Ask the user what difficulty level they want */
-   private void openNewGameDialog() {
-      new AlertDialog.Builder(this)
-           .setTitle(R.string.new_game_title)
-           .setItems(R.array.difficulty,
-            new DialogInterface.OnClickListener() {
-               public void onClick(DialogInterface dialoginterface,
-                     int i) {
-                  startGame(i);
-               }
-            })
-           .show();
-   }
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Music.stop(this);
 
-   /** Start a new game with the given difficulty level */
-   private void startGame(int i) {
-      Log.d(TAG, "clicked on " + i);
-      Intent intent = new Intent(this, Game.class);
-      intent.putExtra(Game.KEY_DIFFICULTY, i);
-      startActivity(intent);
-   }
+//		if (false != newGameClick) {
+//			newGameDialogOpen = true;
+//		}
+
+//		if (false != newGameDialogOpen) {
+//			
+//		}
+	}
+
+	public void onClick(View v) {			
+		switch (v.getId()) {
+		case R.id.continue_button:
+			startGame(Game.DIFFICULTY_CONTINUE);
+			break;
+		// ...
+		case R.id.about_button:
+			Intent i = new Intent(this, About.class);
+			startActivity(i);
+			break;
+		// More buttons go here (if any) ...
+		case R.id.new_button:
+//			newGameClick = true;
+			openNewGameDialog();
+			break;
+		case R.id.exit_button:
+			finish();
+			break;
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.settings:
+			startActivity(new Intent(this, Prefs.class));
+			return true;
+			// More items go here (if any) ...
+		}
+		return false;
+	}
+
+	/** Ask the user what difficulty level they want */
+	private void openNewGameDialog() {
+		// Set flag
+		newGameDialogOpen = true;
+		
+		new AlertDialog.Builder(this)
+				.setTitle(R.string.new_game_title)
+				.setItems(R.array.difficulty,
+						new DialogInterface.OnClickListener() {
+							public void onClick(
+									DialogInterface dialoginterface, int i) {
+								// Reset flag
+								newGameDialogOpen = false;
+								
+								startGame(i);
+							}
+						}).show();
+	}
+
+	/** Start a new game with the given difficulty level */
+	private void startGame(int i) {
+		Log.d(TAG, "clicked on " + i);
+		Intent intent = new Intent(this, Game.class);
+		intent.putExtra(Game.KEY_DIFFICULTY, i);
+		startActivity(intent);
+	}
 }
