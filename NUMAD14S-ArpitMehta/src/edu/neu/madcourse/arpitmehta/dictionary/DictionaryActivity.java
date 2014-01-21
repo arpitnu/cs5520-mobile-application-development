a414075b9384becfb80f57f37e2cad9ff46f0da6
arpitm <arpit231087@gmail.com>
2014-01-21 16:38:14 -0500

    Changes:   1) Minor changes to the activity_dictionary.xml layout.

--- NUMAD14S-ArpitMehta/src/edu/neu/madcourse/arpitmehta/dictionary/DictionaryActivity.java
+++ NUMAD14S-ArpitMehta/src/edu/neu/madcourse/arpitmehta/dictionary/DictionaryActivity.java
@@ -4,6 +4,9 @@ import edu.neu.madcourse.arpitmehta.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class DictionaryActivity extends Activity {
 
@@ -18,6 +21,51 @@ public class DictionaryActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.dictionary, menu);
 		return true;
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		
+		// Clear EditText Field
+		EditText et = (EditText) findViewById(R.id.etDictionary);
+		et.setText("");
+		
+		// Clear Word suggestions Display Field
+		LinearLayout ll = (LinearLayout) findViewById(R.id.llWordView);
+		ll.removeAllViews();
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+	
+	/**
+	 * exitDictionary
+	 * 		Exit the Dictionary activity and return to the main activity.
+	 * 
+	 * @param view
+	 * 
+	 * @return void
+	 */
+	public void exitDictionary(View view) {
+		finish();
 	}
 
 }
