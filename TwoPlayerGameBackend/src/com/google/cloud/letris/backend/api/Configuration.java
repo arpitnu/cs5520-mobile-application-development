3250fa535ae05125b4405b0a615b54674b36e62c
arpitm <arpit231087@gmail.com>
2014-03-21 21:21:30 -0400

    Changes:   2) Added configuration.java

---
+++ TwoPlayerGameBackend/src/com/google/cloud/letris/backend/api/Configuration.java
@@ -0,0 +1,33 @@
+package com.google.cloud.letris.backend.api;
+
+/**
+ * API & Services Configuration
+ */
+public class Configuration {
+	/**
+	   * API name.
+	   */
+	  static final String NAME = "Letris";
+	  
+	  /**
+	   * API version.
+	   */
+	  static final String VERSION = "v1.0";
+	  
+	  /**
+	   * API description.
+	   */
+	  static final String DESCRIPTION = "Letris API";
+	  
+	  /**
+	   * Settings for @ApiNamespace.
+	   */
+	  static final String NAMESPACE_OWNER_DOMAIN = "google.com";
+	  static final String NAMESPACE_OWNER_NAME = "google.com";
+	  static final String NAMESPACE_PACKAGE_PATH = "cloud.letris.client";
+	  
+	  /**
+	   * Web Client ID.
+	   */
+	  static final String WEB_CLIENT_ID = "Your Web Client ID";
+}
